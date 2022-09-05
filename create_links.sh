3f42e0a06b808f7eb576df3bd2aaa01c1f9a26e3
Evrixol <56212925+Evrixol@users.noreply.github.com>
2022-09-05 16:12:52 -0600

    Refined and debugged `create_links.sh` shell script. File now works as expected.

--- create_links.sh
+++ create_links.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #   This file creates symbolic links in the users (assumed) home directory (referencing env variable
 # $HOME) to link files such as .bashrc, .bash_aliases, .profile, and other such so-called "dot-
@@ -8,23 +8,19 @@
 ## linux-gnu
 ## msys
 ## cygwin
-## freebsd
-## darwin
-## win32 (assumed)
 
-toLink=(".bashrc", ".bash_aliases", ".vimrc", ".vim", ".gitconfig")
-linkTo=("bashrc", "bash_aliases", "vimrc", "vim", "git_config")
+linkFiles=(".bashrc" ".bash_aliases" ".vimrc" ".gitconfig" ".vim")
+fileActual=("bashrc" "bash_aliases" "vimrc" "git_config" "vim")
 
 case "$OSTYPE" in
 	"linux-gnu")
-		for i in $toLink
+		for i in ${!fileActual[@]}
 		do
-			ln -s "~/.dotfiles/${linkTo[$i]}" "~/${toLink[$i]}"
+			ln -s "${HOME}/.dotfiles/${fileActual[$i]}" "${HOME}/${linkFiles[$i]}"
 		done;;
 	*) 
 		echo "The operating system type is either unknown or the environment variable \"\$OSTYPE\" is empty."
 		echo "Forcefully exiting script.";
 		exit;;
 esac
-
 
